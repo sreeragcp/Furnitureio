2a93a692ff63ea85780c493aa4e41bd95a54162d
Sreerag C P <Sreeragkunnothuparamba@gmail.com>
2023-07-12 18:51:03 +0530

    bug fixes

--- index.js
+++ index.js
@@ -15,10 +15,10 @@ const session = require("express-session");
 const Razorpay=require('razorpay');
 
 
-var instance = new Razorpay({
-  key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
-});
+// var instance = new Razorpay({
+//   key_id: process.env.RAZORPAY_KEY_ID,
+//   key_secret: process.env.RAZORPAY_KEY_SECRET,
+// });
 
 //session
 app.use(session({
